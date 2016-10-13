688aa25e2aa09c7246b0d180029db1d4e6dd3044
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-10-13 06:19:54 -0500

    añadiendo comprobación de código

--- js/general.js
+++ js/general.js
@@ -2,9 +2,9 @@ $(document).ready(cargar);
 function cargar(){
 	$("#input-celular").keypress(onKeydown);
 	$("#input-celular").keyup(onKeyup);
-	var celu =  window.localStorage.getItem("celular");
-	$("#num").text(celu);
+	$("#num").text(window.localStorage.getItem("celular"));
 }
+
 function onKeydown(evento){
 	var ascii = evento.keyCode;
 	if((ascii>=48 && ascii<=57) || ascii==37 || ascii==39 || ascii<=8){
@@ -13,6 +13,7 @@ function onKeydown(evento){
 		return false;
 	}
 }
+
 function onKeyup(evento){
 	var longitud = $("#input-celular").val().length;
 		if (longitud == 9) {
@@ -21,19 +22,26 @@ function onKeyup(evento){
 			$("#obtener-codigo").removeAttr("href");
 		}
 }
+
 $("#obtener-codigo").click(generarCodigo);
 function generarCodigo(){
 	var longitud = $("#input-celular").val().length;
 
 	if(longitud == 9){
 		window.localStorage.setItem("numeroAleatorio", Math.round(Math.random()*900)+99);
-		var numeroAleatorio = window.localStorage.getItem("numeroAleatorio");
-		var codigo = "LAB" + numeroAleatorio;
-		alert(codigo);
+		alert("LAB" + window.localStorage.getItem("numeroAleatorio"));
 		window.localStorage.setItem("celular", $("#input-celular").val());
 	}
 }
-/*$("#codigo-comprobado").click(comprobarCodigo);
-function comprobarCodigo(){
 
-}*/
+$("#codigo-comprobado").click(comprobarCodigo);
+$("#input-codigo").keypress(onKeydown);
+function comprobarCodigo(){
+	var codigo = $("#input-codigo").val();
+	if(codigo === window.localStorage.getItem("numeroAleatorio")){
+			$("#codigo-comprobado").attr("href", "registro-datos.html");
+		} else {
+			$("#codigo-comprobado").removeAttr("href");
+		}
+}
+
