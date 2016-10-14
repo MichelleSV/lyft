fb321944ebfe0da3326660c99f75bfc585c420b2
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-10-14 17:31:02 -0500

    corrgiendo input-celular, validando código, añadiendo botón para reenviar el código, validando campo del código

--- js/general.js
+++ js/general.js
@@ -1,11 +1,13 @@
 $(document).ready(cargar);
 function cargar(){
-	$("#input-celular").keypress(onKeydown);
+	$("#input-celular").keypress(onKeypress);
 	$("#input-celular").keyup(onKeyup);
 	$("#num").text(window.localStorage.getItem("celular"));
+	$("#input-celular").focus();
+	$(".input-codigo").eq(0).focus();
 }
 
-function onKeydown(evento){
+function onKeypress(evento){
 	var ascii = evento.keyCode;
 	if((ascii>=48 && ascii<=57) || ascii==37 || ascii==39 || ascii<=8){
 		return true;
@@ -26,34 +28,44 @@ function onKeyup(evento){
 $("#obtener-codigo").click(generarCodigo);
 function generarCodigo(){
 	var longitud = $("#input-celular").val().length;
-
 	if(longitud == 9){
 		window.localStorage.setItem("numeroAleatorio", Math.round(Math.random()*900)+99);
-		alert("LAB" + window.localStorage.getItem("numeroAleatorio"));
+		alert("LAB - " + window.localStorage.getItem("numeroAleatorio"));
 		window.localStorage.setItem("celular", $("#input-celular").val());
 	}
 }
 
 $("#codigo-comprobado").click(comprobarCodigo);
-$(".input-codigo").keypress(onKeydown);
+$(".input-codigo").keypress(onKeypress);
 function comprobarCodigo(){
-	var codigo1 = $("#cod1").val();
-	var codigo2 = $("#cod2").val();
-	var codigo3 = $("#cod3").val();
+	var codigo1 = $(".input-codigo").eq(0).val();
+	var codigo2 = $(".input-codigo").eq(1).val();
+	var codigo3 = $(".input-codigo").eq(2).val();
 	var codigo = codigo1 + codigo2 + codigo3;
-	if(codigo === window.localStorage.getItem("numeroAleatorio")){
+	if(codigo == window.localStorage.getItem("numeroAleatorio")){
 			$("#codigo-comprobado").attr("href", "registro-datos.html");
 		} else {
 			$("#codigo-comprobado").removeAttr("href");
+			alert("Código erróneo");
 			$(".input-codigo").val("");
-			$("#cod1").focus();
+			$(".input-codigo").eq(0).focus();
 		}
 }
 
-$(".input-codigo").keypress(onKeydown);
-$(".input-codigo").keydown(cambiarInput);
-function cambiarInput(){
-	if($(this).val().length == 1){
+$(".input-codigo").keypress(onkeyupCodigo);
+function onkeyupCodigo(evento){
+	var ascii = evento.keyCode;
+	if(ascii>=48 && ascii<=57){
 		$(this).next().focus();
 	}
+	else if(ascii==8){
+		$(this).prev().focus();
+	}
+}
+
+$("#reenviar-codigo").click(generarCodigo2);
+function generarCodigo2(){
+	window.localStorage.setItem("numeroAleatorio", Math.round(Math.random()*900)+99);
+	alert("LAB - " + window.localStorage.getItem("numeroAleatorio"));
+	$(".input-codigo").eq(0).focus();
 }
