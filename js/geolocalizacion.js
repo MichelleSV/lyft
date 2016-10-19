37f49fadfb065ff05cf457e782f6d0569857cc7e
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-10-18 19:16:21 -0500

    subiendo maps

--- js/geolocalizacion.js
+++ js/geolocalizacion.js
@@ -9,14 +9,45 @@ var funcionExito = function(posicion) {
 	var map = new GMaps({
 		div: "#map",
 		lat: lat,
-		lng: lon
+		lng: lon,
+		zoom:16
+	});
+	var geocoder = new google.maps.Geocoder;
+	var infowindow = new google.maps.InfoWindow;
+	$("#submit").on('click', function() {
+	geocodeLatLng(geocoder, map, infowindow);
 	});
 	map.addMarker({
 		lat: lat,
 		lng: lon,
+		title:"Estás aquí"
+	});
+
+	var content = $("#direccion");
+	var dir = "";
+	var latlng = new google.maps.LatLng(lat, lon);
+	geocoder = new google.maps.Geocoder();
+	geocoder.geocode({"latLng": latlng}, function(resultado, estado){
+		if (estado == google.maps.GeocoderStatus.OK){
+			if (resultado[0]){
+				dir = resultado[0].formatted_address;
+			}
+			else{
+				dir = "No se ha podido obtener ninguna dirección en esas coordenadas.";
+			}
+		}
+		else{
+			dir = "El Servicio de Codificación Geográfica ha fallado con el siguiente error: " + estado;
+		}
+		content.text(dir);
 	});
 }
 var funcionError = function (error) {
 	console.log(error);
+}
+var cargar = function(){
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(exitoUbicacion,errorUbicaion)
+	}
 }
 $(document).ready(cargarPagina);
